6fdf28da4e0412146496aae0d1a3ba523600ed5f
huespixelstorm <parantargretchgeen@gmail.com>
2026-06-21 23:29:41 +0800

    WIP

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const answerBtn = document
+
 const optionOne = document.querySelector("#optionOne");
 const optionTwo = document.querySelector("#optionTwo");
 const optionThree = document.querySelector("#optionThree");
@@ -19,6 +21,7 @@ optionFour.addEventListener("click", () => {
     announceResult.textContent = "Wrong answer"
 })
 
+
 const mathQ1 = {
     question: "2 x 4 = ?",
     choices: ["2", "4", "6", "8"],
@@ -30,8 +33,27 @@ const mathQ2 = {
     correctAns: "25"
 }
 
+const mathQuestions = [mathQ1, mathQ2]
+
+
+const startBtn = document.querySelector("#start");
+
+let selectedQuestion
+
+startBtn.addEventListener("click", () => {
+    
+    selectedQuestion = getQuestion()
+
+    question.textContent = selectedQuestion.question
+    optionOne.textContent = selectedQuestion.choices[0]
+    optionTwo.textContent = selectedQuestion.choices[1] 
+    optionThree.textContent = selectedQuestion.choices[2] 
+    optionFour.textContent = selectedQuestion.choices[3] 
+})
+
+
 const question = document.querySelector("#question");
 
-function getQuestion(questionSet) {
-    question.textContent = `${question}`
+function getQuestion() {
+    return mathQuestions[Math.floor(Math.random() * mathQuestions.length)]
 }
