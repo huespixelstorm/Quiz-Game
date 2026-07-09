a4283924566c05cf0c7ce2c6ad536b88592933d8
huespixelstorm <parantargretchgeen@gmail.com>
2026-07-09 08:12:09 +0800

    Add files via upload

--- questions.js
+++ questions.js
@@ -15,4 +15,22 @@ const mathQuestions = [{
     question: "102 + 29 = ?",
     choices: ["10229", "131", "2958", "132"],
     correctAns: "131",
+}, 
+{
+    theme: "Math",
+    question: "What is 25% of 80?",
+    choices: ["20", "22", "25", "40"],
+    correctAns: "20"
+},
+{
+    theme: "Math",
+    question: "-1 - 3 = ?",
+    choices: ["4", "-2", "-4", "2"],
+    correctAns: "-4"
+},
+{
+    theme: "Math",
+    question: "5 - 4 = ?",
+    choices: ["1", "6", "-1", "-6"],
+    correctAns: "1",
 }]
