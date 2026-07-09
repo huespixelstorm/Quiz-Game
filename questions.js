379e6e279cd11b365bf6e3daef5fe154cbb7cd56
huespixelstorm <parantargretchgeen@gmail.com>
2026-07-09 08:02:53 +0800

    Separated the Questions and Functions

---
+++ questions.js
@@ -0,0 +1,18 @@
+const mathQuestions = [{
+    theme: "Math",
+    question: "2 x 4 = ?",
+    choices: ["2", "4", "6", "8"],
+    correctAns: "8"
+},
+{
+    theme: "Math",
+    question: "5 x 5 = ?",
+    choices: ["5", "10", "25", "55"],
+    correctAns: "25"
+},
+{
+    theme: "Math",
+    question: "102 + 29 = ?",
+    choices: ["10229", "131", "2958", "132"],
+    correctAns: "131",
+}]
