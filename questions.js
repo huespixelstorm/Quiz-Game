effd1e28b3a37dc82bda2a0ea989e2d50bbc1160
huespixelstorm <parantargretchgeen@gmail.com>
2026-07-18 20:38:48 +0800

    just added some more questions

--- questions.js
+++ questions.js
@@ -33,4 +33,35 @@ const mathQuestions = [{
     question: "5 - 4 = ?",
     choices: ["1", "6", "-1", "-6"],
     correctAns: "1",
-}]
+},
+{
+    theme: "Math",
+    question: "5 * 5 = ?",
+    choices: ["25", "10", "1", "20"],
+    correctAns: "25"
+},
+{
+    theme: "Math",
+    question: "100 ÷ 50 = ?",
+    choices: ["2", "50", "150", "100"],
+    correctAns: "2",
+},
+{
+    theme: "Math",
+    question: "100 x 50 = ?",
+    choices: ["5000", "150", "500", "50000"],
+    correctAns: "5000",
+},
+{
+    theme: "Math",
+    question: "7 * 4 = ?",
+    choices: ['3', "12", "11", "28"],
+    correctAns: "28",
+},
+{
+    theme: "Math",
+    question: "9 x 4 = ?",
+    choices: ["49", "36", "15", "24"],
+    correctAns: "36",
+}
+]
