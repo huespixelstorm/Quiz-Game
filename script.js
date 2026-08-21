08640f3639c75dfc34f53cb95a48526bf8540ecb
huespixelstorm <parantargretchgeen@gmail.com>
2026-08-21 10:43:33 +0800

    rewriting code (unfinished)

--- script.js
+++ script.js
@@ -5,137 +5,56 @@ const optionTwo = document.querySelector("#optionTwo");
 const optionThree = document.querySelector("#optionThree");
 const optionFour = document.querySelector("#optionFour");
 
+const theme = document.querySelector("#theme");
+const gameruleBtn = document.querySelector("#gamerule");
 const announceResult = document.querySelector("#result");
 
 const score = document.querySelector("#score");
 let scoreCounter = 0
 
-optionOne.disabled = true
-optionTwo.disabled = true
-optionThree.disabled = true
-optionFour.disabled = true
+answerBtn.forEach(button => {
+    button.disabled = true;
+});
 
-answerBtn.forEach((button) => {
-    button.addEventListener("click", () => {
-        getResults(button)
-        console.log(button.textContent) 
-    })
-})
+let gamerule = 0    //  gamemode 0 = Start, 1 = playing, 2 = End
 
-function getResults(button) {
+gameruleBtn.addEventListener("click", () => {
+    if (gameruleBtn.textContent === "START") {
+            
+        startPhase()
+            return
+    } else {
 
-    optionOne.disabled = true
-    optionTwo.disabled = true
-    optionThree.disabled = true
-    optionFour.disabled = true
+        if (gameruleBtn.textContent === "Go!") {
+            gamerule = 1
 
-    startBtn.disabled = false
-    startBtn.textContent = `Next`
-
-    if(button.textContent === selectedQuestion.correctAns) {
-        scoreCounter++
-        announceResult.textContent = `Correct! Answer is ${selectedQuestion.correctAns}`
-        score.textContent = `Score: ${scoreCounter}`
-    } else { 
-        announceResult.textContent = `Wrong Answer. It's ${selectedQuestion.correctAns}`
-    }
-}
-
-
-const theme = document.querySelector("#theme");
-const startBtn = document.querySelector("#start");
-
-let selectedQuestion
-
-startBtn.addEventListener("click", () => {      //  START / NEXT / RESET BTN //
-
-    if (startBtn.textContent === "Reset") {
-        endGame()
-        reset()
-        return
-    }
-
-    if (questionCounter === 3) {
-        startBtn.textContent = "Reset"
-        return
-
-    } else { 
-
-    if (startBtn.textContent === "Next" || startBtn.textContent === "START") {
-
-        announceResult.textContent = "-"
-
-        optionOne.disabled = false
-        optionTwo.disabled = false
-        optionThree.disabled = false
-        optionFour.disabled = false
-    
-    display()
-
-    startBtn.disabled = true
         }
     }
+
 })
 
-const questionNo = document.querySelector("#questionNo")
-let questionCounter = 0;
+        //  START
+function startPhase() {
 
-function display() {
-{
-        
-    questionCounter++
-    selectedQuestion = getQuestion()
+    gameruleBtn = "Go!"
 
-    question.textContent = selectedQuestion.question
-    optionOne.textContent = selectedQuestion.choices[0]
-    optionTwo.textContent = selectedQuestion.choices[1] 
-    optionThree.textContent = selectedQuestion.choices[2] 
-    optionFour.textContent = selectedQuestion.choices[3] 
+    answerBtn.forEach(button => {
+    button.disabled = false;
+})
 
-    theme.textContent = `Theme: ${selectedQuestion.theme}`
-    questionNo.textContent = `Q${questionCounter}`
+    answerBtn.forEach((button) => {
+        button.addEventListener("click", () => {
+         // Remove selected from every button
+        answerBtn.forEach(btn => {
+            btn.classList.remove("selected");
+        });
 
-    }
-}
+        // Add selected to the one clicked
+        button.classList.add("selected");
 
+        // Remember the choice
+        maxRounds = Number(button.dataset.rounds);
+    });
+});
 
-const question = document.querySelector("#question");
-
-function getQuestion() {
-    return mathQuestions[Math.floor(Math.random() * mathQuestions.length)]
-}
-
-function endGame() {
-
-        optionOne.textContent = "-"
-        optionTwo.textContent = "-"
-        optionThree.textContent = "-"
-        optionFour.textContent = "-"
-
-        question.textContent = `Play Again?`
-
-        announceResult.textContent = `Out of ${questionCounter} questions, you got ${scoreCounter} right.`
-
-        startBtn.textContent = "Reset"  //  RESET //
-}
-
-function reset() {
-
-    optionOne.disabled = true
-    optionTwo.disabled = true
-    optionThree.disabled = true
-    optionFour.disabled = true
-
-    optionOne.textContent = "-"
-    optionTwo.textContent = "-"
-    optionThree.textContent = "-"
-    optionFour.textContent = "-"
-
-    scoreCounter = 0
-    score.textContent = `Score: ${scoreCounter}`
-
-    questionCounter = 0
-    questionNo.textContent = `Q${questionCounter}`
-
-    startBtn.textContent = "START"
 }
